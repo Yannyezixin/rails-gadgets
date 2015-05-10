83ab5e45b4e647c9398ac2f52cd2d4363d907f8a
Yannyezixin <646840080@qq.com>
2015-05-10 16:23:43 +0800

    add static resources

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,42 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+ready = function () {
+    $('#express').on('submit', function (event) {
+        $msg = $('.msg');
+        $msg.text("");
+        $('.loading').show();
+        $list = $('#order_list tbody');
+        $list.html("");
+        event.preventDefault();
+        console.log($(this).attr('action'));
+
+        orderId = $('#order_id').val();
+        $.post($(this).attr('action'), {"authenticity_token": $('meta[name="csrf-token"]').attr('content'), "order_id": orderId},
+            function (data) {
+                $('.loading').hide();
+                if (data.errCode == 0) {
+                    if (data.message.length != 0) {
+                        $msg.text(data.message);
+                    } else if (data.data.length == 0) {
+                        $msg.text($msg.data('empty'));
+                    } else if (data.data.length > 0) {
+                        data.data.map(function (item) {
+                            $tr = $('<td></td>');
+                            $tr.append($('<p class="time"></p>').text(item.time));
+                            $tr.append($('<p class="content"></p>').text(item.content));
+                            $list.append($('<tr></tr>').append($tr));
+                        });
+                    }
+                } else {
+                    $msg.text(data.message);
+                }
+            }
+        );
+    });
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
